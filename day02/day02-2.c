957a1ab9bd125eaa593cee155c7054aea6577e0f
Euntaek Oh <etfactory@icloud.com>
2024-09-12 13:45:35 +0900

    Changes

--- day02/day02-2.c
+++ day02/day02-2.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main() {
+    int number[2];
+    int result=0;;
 
+    printf("숫자 두 개를 입력하세요\n");
+    scanf_s("%d", &number[0]);
+    scanf_s("%d", &number[1]);
+    for(int i=number[0];i<=number[1];i++) {
+        result = result+i;
+    }
+    printf("%d", result);
 }
