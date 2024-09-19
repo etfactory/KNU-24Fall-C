69d644f68974650b0e354ad5e390bc95478d48ed
Euntaek Oh <etfactory@icloud.com>
2024-09-19 14:14:22 +0900

    03-1 Edit

--- day03/day03-1.c
+++ day03/day03-1.c
@@ -5,6 +5,7 @@
  * 소수이면 1을 반환하고, 아니면 0을 반환한다.
  *
  * 소수: 1과 자기 자신만으로 나누어 떨어지는 수
+ *      양의 약수가 두 개인 수
  * -> 숫자가 1 이하 => 소수가 아님
  * -> 2부터 n/2까지 나누어 떨어지는 수가 있다 => 소수가 아님
  */
@@ -25,11 +26,11 @@ void main() {
 int checkPrimeNumber(int inputNumber) {
     if (inputNumber <= 1) {
         return 0;
-    }
+    } //1은 소수가 아닙니다.
     for (int i = 2; i <= inputNumber / 2; i++) {
         if (inputNumber % i == 0) {
             return 0;
         }
-    }
+    } //inputNumber / 2로 지정한 이유는 2로 나눠진다면 소수가 아니기 때문.
     return 1;
 }
