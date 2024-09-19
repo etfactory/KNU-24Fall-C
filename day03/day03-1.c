9951dd6347d5e604320e86c76764ffba3c69f873
Euntaek Oh <etfactory@icloud.com>
2024-09-19 13:46:41 +0900

    03-1 Upload

---
+++ day03/day03-1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/*
+ * 소수인지 아닌지 판별하는 함수
+ * 소수이면 1을 반환하고, 아니면 0을 반환한다.
+ *
+ * 소수: 1과 자기 자신만으로 나누어 떨어지는 수
+ * -> 숫자가 1 이하 => 소수가 아님
+ * -> 2부터 n/2까지 나누어 떨어지는 수가 있다 => 소수가 아님
+ */
+
+int checkPrimeNumber(int inputNumber);
+
+void main() {
+    int inputNumber;
+    printf("소수인지 확인할 수를 입력하세요: ");
+    scanf_s("%d", &inputNumber);
+    if(checkPrimeNumber(inputNumber) == 1) {
+        printf("해당 수는 소수입니다.");
+    } else {
+        printf("해당 수는 소수가 아닙니다.");
+    }
+}
+
+int checkPrimeNumber(int inputNumber) {
+    if (inputNumber <= 1) {
+        return 0;
+    }
+    for (int i = 2; i <= inputNumber / 2; i++) {
+        if (inputNumber % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
