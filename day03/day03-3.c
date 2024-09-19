47e420b266d1bee0a7c8829f9de9860d0fdb91f1
Euntaek Oh <etfactory@icloud.com>
2024-09-19 14:45:36 +0900

    Upload 03-3

---
+++ day03/day03-3.c
@@ -0,0 +1,10 @@
+#include <math.h>
+#include <stdio.h>
+
+void main() {
+    printf("%d\n", abs(-4));
+    printf("%lf\n", sin(30*(M_PI/180)));
+    printf("%lf\n", cos(30*(M_PI/180)));
+    printf("%lf\n", sqrt(2.5));
+    printf("%lf\n", pow(1.5, 4.0));
+}
