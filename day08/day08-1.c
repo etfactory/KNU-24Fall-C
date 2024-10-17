7221d5238d8fce9f837b424b4060f399c80cf8fd
Euntaek Oh <etfactory@icloud.com>
2024-10-17 14:18:10 +0900

    Edit

--- day08/day08-1.c
+++ day08/day08-1.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
 
-int main(void) {
+int main() {
     char *inputString = (char *)malloc(sizeof(char));
     //포인터 p와 q 문자열 시작위치로
     char *p = inputString;
@@ -10,7 +9,7 @@ int main(void) {
     char temp;
 
     printf("문자열을 입력하세요:");
-    gets(inputString);
+    scanf_s("%s", inputString);
 
     //p포인터를 문자열 끝으로
     while(*p) {
@@ -20,23 +19,11 @@ int main(void) {
 
     //문자열 뒤집기
     while (q<p) {
-        //q포인터가 알파벳이 아닌 경우 q포인터를 증가시킴
-        while(q<p&&!isalpha(*q)) {
-            q++;
-        }
-        //p포인터가 알파벳이 아닌 경우 p포인터를 감소시킴
-        while(q<p&&!isalpha(*p)) {
-            p--;
-        }
-
-        //q포인터가 p포인터보다 작은 경우 두 포인터가 가리키는 문자를 교환
-        if(q<p) {
-            temp = *q;
-            *q = *p;
-            *p = temp;
-            q++;
-            p--;
-        }
+        temp = *q;
+        *q = *p;
+        *p = temp;
+        q++;
+        p--;
     }
 
     printf("뒤집어진 문자열:%s\n", inputString);
--- day08/day08-2.c
+++ day08/day08-2.c
@@ -6,7 +6,7 @@
 void fillRandom(int array[SIZE][SIZE]){
     for(int i=0;i<SIZE;i++){
         for(int j=0;j<SIZE;j++){
-            array[i][j] = rand()%20;
+            array[i][j] = rand()%20+1;
         }
     }
 }
@@ -20,25 +20,19 @@ void printArray(int array[SIZE][SIZE]){
     }
 }
 
-//2차원 배열을 1차원 배열처럼 처리할 수 있기에 포인터 사용
+//void형 포인터 왜 써야 하는지 진짜 모르겠는데요?
 void movePointer(void* array){
-    int* pnt = array;
-    int index = 0;
-    int tmp = 0;
-    for (int i = 0; i < SIZE * SIZE; i++) {
-        // pnt와 배열의 시작 주소 사이의 차이
-        index = pnt - (int*)array;
-        // index / SIZE는 row 위치를, index % SIZE는 column의 위치를 나타내줌
-        printf("현재 위치: (%d, %d), 배열의 값: %d\n", index / SIZE, index % SIZE, *pnt);
-        // if문에 *pnt 그대로 쓰면 0으로 출력되어서 tmp에 저장
-        tmp = *pnt;
-        pnt += *pnt;
-        // 이동할 수 없을 때를 체크
-        if (pnt >= (int*)array + SIZE * SIZE) {
+    int* ptr = (int*)array;
+    int nextIndex = 0;
+    for(int i=0;i<SIZE*SIZE;i++){
+        nextIndex = i + *(ptr+i) - 1;
+        printf("현재 위치 : (%d, %d), 배열의 값 : %d\n", i/SIZE, i%SIZE, *(ptr+i));
+        if(nextIndex >= SIZE*SIZE){
             printf("더 이상 이동할 수 없습니다.\n");
-            printf("종료 위치: (%d, %d), 배열의 값: %d", index / SIZE, index % SIZE, tmp);
+            printf("종료 위치 : (%d, %d), 배열의 값 : %d\n", i/SIZE, i%SIZE, *(ptr+i));
             break;
         }
+        i = nextIndex;
     }
 }
 
