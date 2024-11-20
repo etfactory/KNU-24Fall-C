2bb2e40c9e8791ff0b406af7c15f7ecb910b3a47
Euntaek Oh <etfactory@icloud.com>
2024-11-20 22:27:24 +0900

    11

---
+++ day11/day11-1.c
@@ -0,0 +1,34 @@
+// 이차방정식의 근 계산
+
+#include <stdio.h>
+#include <math.h>
+
+int quadraticEquation(double a, double b, double c, double* x1, double* x2) {
+    double inSqrt = b*b-4*a*c;
+    if (inSqrt < 0) {
+        return 0;
+    } else {
+        *x1 = (-b+sqrt(inSqrt))/(2*a);
+        *x2 = (-b-sqrt(inSqrt))/(2*a);
+        return 2;
+    }
+}
+
+int main() {
+    double a, b, c;
+    double x1, x2;
+    printf("ax^2+bx+c에서 a, b, c의 값을 입력하세요:");
+    scanf_s("%lf %lf %lf", &a, &b, &c);
+
+    int result = quadraticEquation(a,b,c,&x1,&x2);
+
+    if (result == 0) {
+
+    } else if (x1 == x2) {
+        printf("x = %.2lf \n", x1);
+    } else {
+        printf("x = %.2lf or %.2lf \n", x1, x2);
+    }
+
+    return 0;
+}
---
+++ day11/day11-2.c
@@ -0,0 +1,40 @@
+//구분구적법
+//함수 f(x)가 주어졌을 때 구간 [a,b]에서 f(x)의 면적을 구하는 방법
+//구간 [a,b]를 n개의 구간으로 나누고 각 구간의 넓이를 구하여 더함
+//이 때 n을 늘릴 수록 실제 넓이로 수렴
+//n을 두 배씩 늘리면서 적분값을 확인
+//f(x)=-log10(1/x)+sin(x)일 때 [10,30]에서의 적분값을 구하시오
+#include <stdio.h>
+#include <math.h>
+
+double f(double x) {
+    return -log10(1/x)+sin(x);
+}
+
+double integral(double a, double b, int n) {
+    double dx = (b-a)/n;
+    double sum = 0;
+    for (int i=0; i<n; i++) {
+        sum += f(a+i*dx)*dx;
+    }
+    return sum;
+}
+
+int main() {
+    double a = 10;
+    double b = 30;
+    int n = 1;
+    int i = 0;
+    double result = integral(a,b,n);
+    double prevResult = result;
+    while(1) {
+        if (i==30) {
+            break;
+        }
+        n *= 2;
+        result = integral(a,b,n);
+        printf("구간\t%d\t 적분값:%.4lf\n", n, result);
+        i++;
+    }
+    return 0;
+}
